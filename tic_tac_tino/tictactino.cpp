84895b2ca6f4fc640c75624f3daca125e33dcbb5
Ali Trieschmann <ali.trieschmann@gmail.com>
2013-05-14 01:51:34 +0200

    Debugging Code entfernt Kommentare ergänzt

--- tic_tac_tino/tictactino.cpp
+++ tic_tac_tino/tictactino.cpp
@@ -20,45 +20,73 @@ namespace tictactino {
   void set(Player p);
   Player player();
   
+  /*
+    *
+    * Board initialisieren
+    * greenPin, redPin - digitale Eingaenge, an denen die Taster haengen
+    * dataPin, clockPin, latchPin - digit. Ausgaenge, die drei 8 Bit Shift
+    *                               Register steuern (74HC595)
+  */
   void init(int greenPin, int redPin, int dataPin, int clockPin, int latchPin)
   {
-    inputPins[GREEN] = greenPin; inputPins[RED] = redPin;
-    dPin = dataPin; cPin = clockPin; lPin = latchPin;
+    inputPins[GREEN] = greenPin;
+    inputPins[RED] = redPin;
+    dPin = dataPin;
+    cPin = clockPin;
+    lPin = latchPin;
     pinMode(lPin, OUTPUT);
-    pinMode(dPin, OUTPUT);  
+    pinMode(dPin, OUTPUT);
     pinMode(cPin, OUTPUT);
-
-    #ifdef _DEBUG
-    Serial.begin(9600);
-    Serial.println("================"); Serial.println("| TIC TAC TINO |");
-    Serial.println("================"); Serial.println();
-    #endif
+    pinMode(inputPins[GREEN], INPUT);
+    pinMode(inputPins[RED], INPUT);
   }
   
+  /*
+    *
+    * Board fuer neues Spiel vorbereiten
+    *
+  */
   void reset()
-  {
-    blinkmask = cursor = 1;
+  { 
+    // blinkmask - Bitmaske fuer blinkende Darstellung
+    blinkmask = 1;
+    cursor = 1;
+    // Bitmuster, das zum Sieg gefuehrt hat (nach Spielende gesetzt)
     winpattern = 0;
     lastinput = LOW;
     blinktoggle = false;
-    playground[GREEN] = playground[RED] = 0;
-    inputtimer = blinktimer = 0;
+    // Spielfeld-Bitmap der einzelnen Spieler
+    playground[GREEN] = 0;
+    playground[RED] = 0;
+    // Timer-Variablen, die Tasteneingabe und Matrix-Anzeige (blinken) steuern
+    inputtimer = 0;
+    blinktimer = 0;
+    // Spielzugzaehler
     counter = 0;
+    // Register - 24 (!!!) Bit Wort, das in die Shiftregister geschrieben wird
+    // 2 Bit frei; 4 Bit Zugzaehler; 9 Bit Spielfeld rot; 9 Bit Spielfeld gruen
     reg = 0;
     blinktimer = millis() + blinkfreq;
-    #ifdef _DEBUG
-    Serial.println(); Serial.println("-----------"); Serial.println("Neues Spiel");
-    Serial.println("-----------"); Serial.println();
-    #endif
+    // Spielstatus
     status = RESET;
   }
-    
+  
+  /*
+    *
+    * Gibt den aktuellen Spieler zurück
+    *
+  */
   Player player()
   {
     if ((counter & 1) == 0) return GREEN;
     else return RED;
   }
 
+  /*
+    *
+    * Spielen
+    *
+  */
   void play()
   {
     reset();
@@ -70,6 +98,12 @@ namespace tictactino {
     }
   }
   
+  /*
+    *
+    * Eingabe-Schleife - Wechselnde Abfrage der Spielerzuege
+    * bis unentschieden oder Sieg erreicht ist
+    *
+  */
   void turn(Player p)
   {
     switch (read(p)) {
@@ -87,32 +121,43 @@ namespace tictactino {
     refresh();
   }
   
+  /*
+    *
+    * Einlesen der Taster-Eingabe - kurzer Tastendruck bewegt Cursor (blinkende LED)
+    *                               langer Tastendruck setzt Spielzug
+    *
+  */
   Command read(Player p)
   {
     Command cmd = NOOP;
     int input = digitalRead(inputPins[p]);
-    if (lastinput == LOW) {
-      // key press detected
+    if (lastinput == LOW) { // Taster war nicht gedrückt
+      // Tastendruck entdeckt
       if (input == HIGH) {
         lastinput = HIGH;
-        inputtimer = millis() + 1000;
+        inputtimer = millis() + 1000; // Timer starten
       }
-      // else: nothing happened
-    } // key was pressed
-    else if (input == LOW && inputtimer <= millis()) {
-      // and released after long time
+      // nichts passiert
+    }
+    else if (input == LOW && inputtimer <= millis()) { // Taster war gedrueckt worden
+      // nach langem Druck losgelassen -> Zug setzen
       cmd = SET;
       lastinput = LOW;
     }
     else if (input == LOW && inputtimer > millis()) {
-      // or released after short time
+      // nach kurzem Druck losgelassen - Cursor weiterbewegen
       cmd = MOVE;
       lastinput = LOW;
     }
-    delay(50); // entprellen
+    delay(50); // Taster entprellen
     return cmd;
   }
   
+  /*
+    *
+    * Cursor auf naechste moegliche Position setzen
+    *
+  */
   void move(Player p)
   {
     uint16_t mask = (playground[GREEN] | playground[RED]);
@@ -135,44 +180,33 @@ namespace tictactino {
     blinktimer = 0;
   }
   
+  /*
+    *
+    * LED-Matrix auffrischen
+    *
+  */
   void refresh()
   {
     uint32_t mask = 0;
     byte data = 0;
-    /*
-    #ifdef _DEBUG
-    for (int i = 0; i < 9; ++i) {
-      if (i % 3 == 0) {
-        Serial.println();
-        Serial.print("   ");
-      }
-      if (bitRead(blinkmask, i)) Serial.print(" * ");
-      else if (bitRead(playground[GREEN], i)) Serial.print(" X ");
-      else if (bitRead(playground[RED], i)) Serial.print(" O ");
-      else Serial.print(" . ");
-    }
-    Serial.println(); Serial.println();
-    #endif
-    */
-    // Blinkzustand ermitteln und register anpassen
-    if (blinktimer <= millis()) {
-      // register mit Spielstand und Zähler befüllen
+    if (blinktimer <= millis()) { // Blink-Timer abgelaufen -> Matrix neu schreiben
+      // Registervariable mit Spielstand und Zähler befüllen
       reg = ((uint32_t) counter << 18) | ((uint32_t) playground[RED] << 9) | ((uint32_t) playground[GREEN]);
       switch (status) {
         case RUNNING:
-          mask = (uint32_t) blinkmask << (9 * (counter & 1));
+          mask = (uint32_t) blinkmask << (9 * (counter & 1)); // Waehrend des Spiels Cursor anzeigen
           break;
         case EQUAL:
-          mask = reg;
+          mask = reg; // Bei unentschieden alle Felder blinken lassen
           break;
         case WIN_RED:
-          mask = (uint32_t) blinkmask << 9;
+          mask = (uint32_t) blinkmask << 9; // Sieg rot - rote Gewinnreihe blinken lassen
           break;
         case WIN_GREEN:
-          mask = (uint32_t) blinkmask;
+          mask = (uint32_t) blinkmask; // Sieg gruen - gruene Gewinnreihe blinken lassen
           break;
       }
-      Serial.print("Mask: "); Serial.println(mask, BIN);
+      // blinkende LEDs ein- bzw. ausschalten
       if (blinktoggle == true) {
           reg |= mask;
           blinktoggle = false;
@@ -194,6 +228,11 @@ namespace tictactino {
     
   }
   
+  /*
+    *
+    * Spielzug setzen und Spielsituation ueberpruefen
+    *
+  */
   void set(Player p)
   {
     playground[p] |= cursor;
@@ -204,19 +243,8 @@ namespace tictactino {
       if (p == GREEN) cursor = 256;
       else cursor = 1;
       move(p);
-      #ifdef _DEBUG
-      Serial.print("Naechster Zug: ");
-      switch (p) {
-        case GREEN:
-          Serial.println("Gruen");
-          break;
-        case RED:
-          Serial.println("Rot");
-          break;
-      }
-      #endif
     }
-    // check if someone is winner
+    // liegt eine Gewinnsituation vor?
     for (int i = 0; i < 8; ++i) {
       if ((playground[GREEN] & winmask[i]) == winmask[i]) {
         winpattern = winmask[i];
@@ -229,11 +257,12 @@ namespace tictactino {
         break;
       }
     }
-    if (winpattern == 0 && counter >= 8) {
+    if (winpattern == 0 && counter >= 8) { // kein weiterer Zug mehr moeglich und keine Gewinnsituation -> unentschieden
       if (counter == 8) {
-        playground[p] |= cursor;
+        //playground[p] |= cursor;
         set(p);
-        blinkmask = cursor = 0;
+        blinkmask = 0;
+        cursor = 0;
         return;
       }
       else status = EQUAL;
@@ -241,33 +270,16 @@ namespace tictactino {
     blinktimer = 0;
   }
   
-  
+  /*
+    *
+    * Anzeige des Spieausgangs nach Spielende
+    * Warten auf (beliebigen) Tastendruck, um neues Spiel zu starten
+    *
+  */
   void show()
   {
-    #ifdef _DEBUG
-    Serial.println(); Serial.println("-----------"); Serial.println("Spiel zu Ende");
-    Serial.println("-----------"); Serial.println();
-    #endif
     cursor = 0;
     blinkmask = winpattern;
-    //refresh();
-    #ifdef _DEBUG
-    switch (status) {
-      case WIN_RED:
-        Serial.print("Rot hat gewonnen ");
-        break;
-      case WIN_GREEN:
-        Serial.print("Gruen hat gewonnen ");
-        break;
-      case EQUAL:
-        Serial.print("Unentschieden ");
-        break;
-    }
-    Serial.print("nach dem ");
-    Serial.print(counter);
-    Serial.println(" Spielzug");
-    Serial.println("Beliebe Taste druecken, um neues Spiel zu starten ...");
-    #endif
     do {
         refresh();
         if (lastinput == LOW && (digitalRead(inputPins[GREEN]) == HIGH || digitalRead(inputPins[RED]) == HIGH))
--- tic_tac_tino/tictactino.h
+++ tic_tac_tino/tictactino.h
@@ -3,8 +3,6 @@
 
 #include "Arduino.h"
 
-#define _DEBUG 1
-
 namespace tictactino {
   const int blinkfreq = 200; // blinking frequency in ms - should be ~ >150
   void init(int greenPin, int redPin, int dataPin, int clockPin, int latchPin);
